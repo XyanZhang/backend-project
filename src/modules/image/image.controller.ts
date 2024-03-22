8a347760e0228bee0b658f844b613053b9d29789
XyanZhang <1743067175@qq.com>
2024-03-22 18:02:15 +0800

    feat: use sharp demo

--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ImageService } from './image.service';
+import { join } from 'path';
 
 @Controller()
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('image')
+  getHello(@Query() query): string {
     return this.imageService.getImageUrl('');
+  }
+
+  @Get('imageResize')
+  getResizeImage(@Query() query) {
+    let inputPath = join(__dirname, '..', '..', '..', 'public', 'img', '1.jpg');
+    let outputPath = join(__dirname, '..', '..', '..', 'public', 'img', 'test.jpg');
+    this.imageService.resizeImage(inputPath, outputPath, 100, 100);
   }
 }
--- src/modules/image/image.service.ts
+++ src/modules/image/image.service.ts
@@ -1,5 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import * as sharp from 'sharp';
+
+@Injectable()
 export class ImageService {
     getImageUrl(image: string): string {
         return `/images/${image}`;
+    }
+    async resizeImage(inputPath: string, outputPath: string, width: number, height: number) {
+        await sharp(inputPath)
+            .resize(width, height)
+            .toFile(outputPath);
     }
 }
